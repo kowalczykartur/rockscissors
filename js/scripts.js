65b458ed44e012af6ed6f97fd55aeb098b96000a
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-06 20:53:06 +0100

    utworzenie funkcji

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function getTriangleArea(a, h) {
+    if (a > 0 && h > 0)
+    { 
+        return a*h/2;
+    }
+    else {
+        return "Nieprawidłowe dane";
+    }
+        
+}
+console.log(getTriangleArea(10, 6));
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(12, 13);
+var triangle3Area = getTriangleArea(18, 20);
