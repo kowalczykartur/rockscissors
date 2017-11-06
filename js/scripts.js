43359c0bdd967de4d0165b1e767644ecbba9c2ab
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-06 21:05:50 +0100

    utworzono tablice

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
-function getTriangleArea(a, h) {
-    if (a > 0 && h > 0)
-    { 
-        return a*h/2;
-    }
-    else {
-        return "Nieprawidłowe dane";
-    }
-        
+var nameFemale = ['Basia' ,'Kasia', 'Zosia', 'Beata', 'Ola'];
+var nameMale = ['Artur', 'Łukasz', 'Szymon', 'Antek','Adas'];
+var allNames = nameFemale.concat(nameMale);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+ allNames.push (newName) ; 
 }
-console.log(getTriangleArea(10, 6));
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(12, 13);
-var triangle3Area = getTriangleArea(18, 20);
