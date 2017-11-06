8a1ec90c1cf23957bc98bca75a34701d6e1e4a5c
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-06 21:48:21 +0100

    utworzone repo dom1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-function drawTree(size){
-   for (i = 1; i < (size + 1); i++){
-       var star = '';
-       for (j = 0; j < i; j++ ){
-           star += '*';
-           
-       }
-       console.log(star);
-   } 
+var buttons = document.getElementsByClassName('button');
+for (i=0; i < buttons.length; i++){
+   console.log (buttons[i].innerText); 
 }
-drawTree(5);
+
